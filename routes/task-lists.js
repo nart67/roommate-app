8aa6879d2ddbfb37d3d960e84ef236c44d5e7bc9
Dennis Tran <nartdensin@gmail.com>
2018-04-19 22:48:42 -0700

    add merge param so update and delete routes can access parent param

--- routes/task-lists.js
+++ routes/task-lists.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router({mergeParams: true});
 var TaskList = require('../model/task-lists').TaskList;
 
 // Tasks subroute
