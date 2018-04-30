8513b48e1b3cc35e3719555027a71329e092039b
Dennis Tran <nartdensin@gmail.com>
2018-04-29 20:34:12 -0700

    remove list reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
 import auth from './auth'
-import lists from './lists'
 import { reduxReducer as orm } from '../orm/models';
 
 export default combineReducers({
   auth,
-  lists,
   orm
 })
