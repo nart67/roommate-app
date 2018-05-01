8c6e8148988225936b9fdea31cdf4af1acaad16b
Dennis Tran <nartdensin@gmail.com>
2018-04-30 22:06:38 -0700

    add reducer for Task orm model

--- src/orm/models/Task.js
+++ src/orm/models/Task.js
@@ -1,7 +1,21 @@
 import {fk, many, attr, Model} from 'redux-orm';
 
 export class Task extends Model {
-
+    static reducer(action, Task, session) {
+        switch (action.type) {
+        case 'CREATE_TASK':
+            Task.create(action.payload);
+            break;
+        case 'UPDATE_TASK':
+            Task.withId(action.payload.id).update(action.payload);
+            break;
+        case 'REMOVE_TASK':
+            Task.withId(action.payload.id).delete();
+            break;
+        default:
+            break;
+        }
+    }
 }
 Task.modelName = 'Task';
 
