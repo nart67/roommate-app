c9a457f18238c70dac5f91cc1d6dd9306f28b032
Dennis Tran <nartdensin@gmail.com>
2018-04-30 22:10:11 -0700

    add AddTask component and integrate task list with socket for updates

--- src/components/Tasks/ViewTaskList.js
+++ src/components/Tasks/ViewTaskList.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { createTask } from '../../actions/orm';
 import { addList } from '../../actions/lists';
 import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
 import './ViewTaskList.css';
+import AddTask from './components/AddTask';
+import socket from '../../socket/socket'
 
 class ViewTaskList extends Component {    
     constructor(props) {
@@ -21,7 +24,30 @@ class ViewTaskList extends Component {
               return response.json();
             }
         }).then(data => {
-            data && this.props.dispatch(addList(this.list_id, data.tasks));
+            if (data) {
+                this.props.dispatch(addList(this.list_id));
+                for (let i = 0; i < data.tasks.length; i++) {
+                    const task = data.tasks[i];
+                    this.props.dispatch(createTask(task));
+                }
+                this.connectSocket();
+            }
+        });
+    }
+
+    connectSocket = () => {
+        const self = this;
+        socket.emit('subscribe', '5add0c6967a0e81a6c65e9ee');
+        socket.on('task', function(data) {
+            console.log(data);
+            switch (data.type) {
+            case 'ADD':
+                console.log('test');
+                self.props.dispatch(createTask(data.task));
+                break;
+            default:
+                break;
+            }
         });
     }
 
@@ -30,10 +56,18 @@ class ViewTaskList extends Component {
           <div className='task-list'>
             <Grid container justify='center' spacing={16}>
                 <Grid item xs={12} sm={8}>
-                <Paper rounded={false}>
+                    <AddTask list={this.list_id} />
+                </Grid>
+                <Grid item xs={12} sm={8}>
+                <Paper rounded='false'>
                     <ul>
-                    {this.props.lists[this.list_id] ? this.props.lists[this.list_id].map((task) => 
-                        <li className='task'>{task.title}</li>
+                    {this.props.lists[this.list_id] ? 
+                    this.props.Task.items.map((id) => 
+                        this.props.Task.itemsById[id]
+                    ).filter((task) =>
+                        task.task_list === this.list_id
+                    ).map((task) =>
+                        <li className='task' key={task.id}>{task.title}</li>
                     ) :
                     null}
                     </ul>
@@ -46,7 +80,8 @@ class ViewTaskList extends Component {
 }
 
 const mapStateToProps = state => ({
-    lists: state.lists
+    lists: state.lists,
+    Task: state.orm.Task
   });
 
 export default connect(mapStateToProps)(ViewTaskList);
---
+++ src/components/Tasks/components/AddTask.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import Grid from 'material-ui/Grid';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import Paper from 'material-ui/Paper';
+import './AddTask.css';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from 'material-ui/styles';
+import NoteAdd from '@material-ui/icons/NoteAdd';
+
+const styles = theme => ({
+    textField: {
+        width: 'calc(100% - 140px)',
+        margin: theme.spacing.unit
+    },
+    button: {
+      margin: theme.spacing.unit,
+      width: '100px'
+    },
+
+    rightIcon: {
+      marginLeft: theme.spacing.unit,
+    }
+  });
+  
+
+class AddTask extends Component {
+
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        value: '',
+      };
+    }
+  
+    handleChange = (event) => {
+      this.setState({
+        value: event.target.value,
+      });
+    };
+
+    submit = () => {
+        var data = new URLSearchParams();
+        data.append('task', JSON.stringify({title: this.state.value}));
+        fetch(`/lists/${this.props.list}/tasks`, {
+          body: data,
+          credentials: 'same-origin',
+          method: 'POST',
+          headers: new Headers({
+            'Content-Type': 'application/x-www-form-urlencoded'
+          })
+        })
+        .then(response => {
+            const contentType = response.headers.get("content-type");
+            if (contentType && contentType.indexOf("application/json") !== -1) {
+              return response.json();
+            }
+        }).then(data => {
+            console.log(data);
+        });
+    }
+  
+    render() {
+      const { classes } = this.props;
+      return (
+        <div className="AddTaskDiv">
+          <Paper rounded='false'>
+          <TextField
+            className={classes.textField}
+            id="AddTaskField"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <Button className={classes.button} variant="raised" color="default" onClick={this.submit}>
+            Add
+            <NoteAdd className={classes.rightIcon} />
+          </Button>
+          </Paper>     
+        </div>
+      );
+    }
+  }
+
+  AddTask.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(AddTask);
