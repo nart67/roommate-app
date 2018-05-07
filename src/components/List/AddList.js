1867ea63f63026c7f2e24d342cacd28f499eee19
Dennis Tran <nartdensin@gmail.com>
2018-05-06 19:20:17 -0700

    new component

---
+++ src/components/List/AddList.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+import Grid from 'material-ui/Grid';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import Paper from 'material-ui/Paper';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from 'material-ui/styles';
+import NoteAdd from '@material-ui/icons/NoteAdd';
+
+const styles = theme => ({
+    textField: {
+        width: 'calc(100% - 140px)',
+        margin: theme.spacing.unit
+    },
+    button: {
+      margin: theme.spacing.unit,
+      width: '100px'
+    },
+
+    rightIcon: {
+      marginLeft: theme.spacing.unit,
+    }
+  });
+  
+
+class AddList extends Component {
+    state = {
+      value: ''
+    }
+
+    constructor(props) {
+      super(props);
+    }
+  
+    handleChange = (event) => {
+      this.setState({
+        value: event.target.value,
+      });
+    };
+
+    submit = () => {
+        var data = new URLSearchParams();
+        data.append('taskList', JSON.stringify({displayName: this.state.value}));
+        fetch(`/groups/${this.props.match.params.id}/lists/`, {
+          body: data,
+          credentials: 'same-origin',
+          method: 'POST',
+          headers: new Headers({
+            'Content-Type': 'application/x-www-form-urlencoded'
+          })
+        })
+        .then(response => {
+            const contentType = response.headers.get("content-type");
+            if (contentType && contentType.indexOf("application/json") !== -1) {
+              return response.json();
+            }
+        }).then(data => {
+            console.log(data);
+        });
+    }
+  
+    render() {
+      const { classes } = this.props;
+      return (
+        <div className="AddTaskDiv">
+          <Paper rounded='false'>
+          <TextField
+            className={classes.textField}
+            id="AddTaskField"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <Button className={classes.button} variant="raised" color="default" onClick={this.submit}>
+            Add
+            <NoteAdd className={classes.rightIcon} />
+          </Button>
+          </Paper>     
+        </div>
+      );
+    }
+  }
+
+  AddList.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(AddList);
