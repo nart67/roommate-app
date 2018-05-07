f57e4a029f986c479a485bbc1abe9583252fc7bc
Dennis Tran <nartdensin@gmail.com>
2018-05-06 19:21:59 -0700

    route to add list component

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import Home from '../components/Home/Home';
 import Login from '../components/Login/Login';
-import ViewTaskList from '../components/Tasks/ViewTaskList';
+import ViewTaskList from '../components/List/ViewTaskList';
+import AddList from '../components/List/AddList';
 
 const AppRouter = () => (
     <div>
@@ -10,6 +11,7 @@ const AppRouter = () => (
         <Route path="/" component={Home} exact={true} />
         <Route path="/login" component={Login} />
         <Route path='/lists/:id' exact component={ViewTaskList} />
+        <Route path='/groups/:id/lists' exact component={AddList} />
       </Switch>
     </div>
 );
