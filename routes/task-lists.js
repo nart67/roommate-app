ea77ae7dfb031d097a719c219f214f09acd7adbd
Dennis Tran <nartdensin@gmail.com>
2018-05-06 19:17:36 -0700

    emit socket events on routes

--- routes/task-lists.js
+++ routes/task-lists.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router({mergeParams: true});
 var TaskList = require('../model/task-lists').TaskList;
 var Group = require('../model/groups').Group;
+var emit = require('../helper/socket').emit;
 
 // Tasks subroute
 router.use('/:listId/tasks', require('./tasks'));
@@ -10,15 +11,19 @@ router.use('/:listId/tasks', require('./tasks'));
 router.put('/:id', function(req, res) {
     var user_id = req.user.id;
     var id = req.params.id;
+    var socket = req.body.socket;
     
     if (!req.body.taskList) {
       missing(res);
       return;
     }
     var newTaskList = JSON.parse(req.body.taskList);
-    TaskList.updateList(id, user_id, newTaskList, function(err) {
+    TaskList.updateList(id, user_id, newTaskList, function(err, newTaskList) {
       if (err) res.status(404).json({message: "Not found"});
-      else res.status(200).json({message: "Update successful"});
+      else {
+        res.status(200).json({message: "Update successful"});
+        emit(groupId, 'list', {type: 'ADD', task: newTaskList}, socket);
+      }
     })
   });
   
@@ -26,12 +31,16 @@ router.put('/:id', function(req, res) {
   router.delete('/:id', function(req, res) {
     var user_id = req.user.id;
     var id = req.params.id;
+    var socket = req.body.socket;
+    var groupId = req.params.groupId;
+
     TaskList.deleteList(id, user_id, function(err, taskList) {
       if (err || !taskList) res.status(404).json({message: "Not found"});
       else {
         Group.findById(req.params.groupId, function(err, group) {
-          group.removeList(newTaskList._id);
+          group.removeList(taskList._id);
           res.status(200).json({message: "Delete successful"});
+          emit(groupId, 'list', {type: 'DELETE', list: taskList}, socket);
         });
       }
     })
@@ -39,9 +48,13 @@ router.put('/:id', function(req, res) {
   
   // Create task list
   router.post('/', function(req, res) {
-    var newTaskList = new TaskList({
-      group: req.params.groupId,
-      displayName: req.body.displayName,
+    var newTaskList = JSON.parse(req.body.taskList);
+    var socket = req.body.socket;
+    var groupId = req.params.groupId;
+
+    newTaskList = new TaskList({
+      ...newTaskList,
+      group: groupId
     });
     newTaskList.save(function(err) {
       if (err) res.status(409).json({message: "Add failed"});
@@ -49,6 +62,7 @@ router.put('/:id', function(req, res) {
         Group.findById(req.params.groupId, function(err, group) {
           group.addList(newTaskList._id);
           res.status(201).json({message: "Add successful", taskList: newTaskList});
+          emit(groupId, 'list', {type: 'ADD', list: newTaskList}, socket);
         });
       }
     });
