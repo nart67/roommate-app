1a42ca7653ba8722b53c8715dd66bc54292bc389
Dennis Tran <nartdensin@gmail.com>
2018-05-09 21:28:33 -0700

    add Channel model to redux orm reducer

--- src/orm/models.js
+++ src/orm/models.js
@@ -1,9 +1,10 @@
 import {fk, many, attr, Model, ORM, createReducer} from 'redux-orm';
-import {Group} from './models/Group'
-import {Task} from './models/Task'
-import {TaskList} from './models/TaskList'
-import {User} from './models/User'
+import {Group} from './models/Group';
+import {Task} from './models/Task';
+import {TaskList} from './models/TaskList';
+import {User} from './models/User';
+import {Channel} from './models/Channel';
 
-const orm = new ORM();
-orm.register(Task, TaskList, Group, User);
+export const orm = new ORM();
+orm.register(Task, TaskList, Group, User, Channel);
 export const reduxReducer = createReducer(orm);
