0ae7942bf9295a8a691bc1febfc7a32c0ebc77c8
Dennis Tran <nartdensin@gmail.com>
2018-05-09 21:29:30 -0700

    add messages reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import auth from './auth'
 import { reduxReducer as orm } from '../orm/models';
 import lists from './lists'
+import messages from './messages'
 
 export default combineReducers({
   auth,
   orm,
-  lists
+  lists,
+  messages
 })
