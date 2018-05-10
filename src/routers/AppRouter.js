ef5a16769fd964bddfc81fc186d6f1b389d578e2
Dennis Tran <nartdensin@gmail.com>
2018-05-09 21:32:20 -0700

    route to AddGroup, AddChannel, and Chat components

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,6 +4,9 @@ import Home from '../components/Home/Home';
 import Login from '../components/Login/Login';
 import ViewTaskList from '../components/List/ViewTaskList';
 import AddList from '../components/List/AddList';
+import AddGroup from '../components/Group/AddGroup';
+import AddChannel from '../components/Channel/AddChannel';
+import Chat from '../components/Chat/Chat';
 
 const AppRouter = () => (
     <div>
@@ -11,7 +14,10 @@ const AppRouter = () => (
         <Route path="/" component={Home} exact={true} />
         <Route path="/login" component={Login} />
         <Route path='/lists/:id' exact component={ViewTaskList} />
-        <Route path='/groups/:id/lists' exact component={AddList} />
+        <Route path='/groups/:id/lists/add' exact component={AddList} />
+        <Route path='/groups/:id/channels/add' exact component={AddChannel} />
+        <Route path='/groups/add' exact component={AddGroup} />
+        <Route path='/channels/:id' exact component={Chat} />
       </Switch>
     </div>
 );
