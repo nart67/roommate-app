f6f6a6d1ec79b911186b39fb9ff6bacb8c1040db
Dennis Tran <nartdensin@gmail.com>
2018-05-20 10:39:53 -0700

    extract API base URL to constants file for easier management

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import { Button, Icon } from 'semantic-ui-react'
+import { API_URL } from '../../constants';
 
 const Login = () => (
   <div>
-    <a href='/api/auth/facebook/'>
+    <a href={API_URL + '/api/auth/facebook/'}>
     <Button color='facebook'>
       <Icon name='facebook' /> Log In with Facebook
     </Button>
     </a>
-    <a href='/api/auth/google/'>
+    <a href={API_URL + '/api/auth/google/'}>
     <Button color='google plus'>
       <Icon name='google plus' /> Log In with Google
     </Button>
