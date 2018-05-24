c669f072634f333fc429ec4a81058c873f759951
Dennis Tran <nartdensin@gmail.com>
2018-05-23 22:35:40 -0700

    change text formatting

--- src/components/Chat/components/Message.js
+++ src/components/Chat/components/Message.js
@@ -2,14 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Message = ({ message, user }) => (
-  <p>
-    <i>{user}</i>: {message}
-  </p>
+    <div className='message-container'>
+      <b>{user}</b>: {message}
+    </div>
 )
 
 Message.propTypes = {
   message: PropTypes.string.isRequired,
-  user: PropTypes.string.isRequired
+  user: PropTypes.string
 }
 
 export default Message
